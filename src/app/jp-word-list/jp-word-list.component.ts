c86bfc32d847721b37a6285b612dd43cd7eca2d9
renzop5231 <renzop5231@gmail.com>
2020-05-17 16:46:06 -0700

    homepage, navigation, words-update

---
+++ src/app/jp-word-list/jp-word-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jp-word-list',
+  templateUrl: './jp-word-list.component.html',
+  styleUrls: ['./jp-word-list.component.css']
+})
+export class JpWordListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/word-list/word-list.component.ts
+++ src/app/word-list/word-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import koreanVerbList from '../../assets/json/KoreanVerbList.json';
+import KoreanAdjectiveList from '../../assets/json/KoreanAdjectiveList.json';
 
 export interface Tile {
   cols: number;
@@ -15,16 +16,40 @@ export interface Tile {
 })
 export class WordListComponent implements OnInit {
 
-  tiles: Tile[] = [
-  ];
+  currentTiles: Tile[] = [];
+
+  verbTiles: Tile[] = [];
+
+  adjectiveTiles: Tile[] = [];
 
   constructor() { }
+
+  setCurrentTiles(tileType:string){
+    switch(tileType) {
+      case 'verbs':
+        this.currentTiles = this.verbTiles;
+        break;
+      case 'adjectives':
+        this.currentTiles = this.adjectiveTiles;
+        break;
+      default:
+        this.currentTiles = this.verbTiles;
+    }
+
+  }
 
   ngOnInit(): void {
     for (let i = 0; i < koreanVerbList.length; i++){
       let wordPair = {text1: koreanVerbList[i].english, text2: koreanVerbList[i].korean, cols: 1, rows: 1};
-      this.tiles.push(wordPair);
+      this.verbTiles.push(wordPair);
     };
+
+    for (let i = 0; i < KoreanAdjectiveList.length; i++){
+      let wordPair = {text1: KoreanAdjectiveList[i].english, text2: KoreanAdjectiveList[i].korean, cols: 1, rows: 1};
+      this.adjectiveTiles.push(wordPair);
+    };
+
+    this.setCurrentTiles('verbs');
     console.log(koreanVerbList);
   }
 
